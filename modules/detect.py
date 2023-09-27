eebcd781839ecaf2d19d2455a4699942de2710b4
Roy <roytad291@gmail.com>
2023-09-27 19:28:50 +0700

    27/9 7:28pm

--- modules/detect.py
+++ modules/detect.py
@@ -3,6 +3,23 @@ import re,json,os
 
 JSON_PATH= os.getcwd()+'/data/file_info.json'
 
+#text color formats
+red = "\033[91m"
+green = "\033[92m"
+yellow = "\033[93m"
+blue = "\033[94m"
+magenta = "\033[95m"
+cyan = "\033[96m"
+blend_color = "\033[38;2;255;0;127m"
+green_text = '\033[92m'
+bright_red = "\033[91m"
+bright_green = "\033[92m"
+bright_yellow = "\033[93m"
+bright_blue = "\033[94m"
+bright_magenta = "\033[95m"
+bright_cyan = "\033[96m"
+bright_white = "\033[97m"
+reset_text = "\033[0m"
 
 SQLI= [
     r'"{1,3}.*?\'+.*?\'+.*?\'{1,3}"',
@@ -39,8 +56,8 @@ def get_nested_dict_length(dictionary):
 
 def sqli(file_path):
     vulnerable=[]
-    isVulnerable=False
-    for line in range(1, get_nested_dict_length(data[file_path])):
+    isVulnerable = False
+    for line in range (1, get_nested_dict_length(data[file_path])):
         string = data[file_path][str(line)]
         for sql in SQLI:
             try:
@@ -48,14 +65,16 @@ def sqli(file_path):
                     isVulnerable=True
                     temp=[file_path,line,string]
                     vulnerable.append(temp)
-                    temp=[]
+                    
             except re.error as e:
                 pass
     if isVulnerable:
         for v in vulnerable: 
-            print(f"The file {vulnerable[0][0]} have been vulnerable to SQL injection attack")
-            print(f"Line {v[1]}")
-            print(f"Vulnerable code snippet:  {v[2]}\n")
+            print("🆘🆘" + red + f" The file {vulnerable[0][0]} have been vulnerable to SQL injection attack" + reset_text)
+            print(cyan + f"  [*] Line {v[1]}")
+            print(f"   [*] Vulnerable code snippet:  {v[2]}\n" + reset_text)
+    else:
+        print(green + f'✅ Your {file_path} is not Vulnerable to SQL Injection! 👌😁👌' + reset_text)
             
             
             
--- wssca.py
+++ wssca.py
@@ -1,24 +1,25 @@
 from modules.utils import *
 from modules.detect import *
+from tqdm import trange
 import argparse,sys,time
 
 
 
-print("""
+print(magenta +"""
 _ _ _ _____ _____ _____ _____ 
-| | | |   __|   __|     |  _  |
-| | | |__   |__   |   --|     |
+| | | |   __|   __|     |  _  |"""+blend_color+"""
+| | | |__   |__   |   --|     |"""+red+"""
 |_____|_____|_____|_____|__|__|
-      
+"""+green+"""
 By Khanhhnahk1 and H Roy Todd
-""")
+""" + reset_text)
 
 
 # Create an ArgumentParser object
-parser = argparse.ArgumentParser(description='Example argument parsing')
+parser = argparse.ArgumentParser(description='\U0001F4D6 Example argument parsing')
 
 
-parser.add_argument('-f', '--file', help='Specify project folter, example: /mnt/d/project/vulnerableflask')
+parser.add_argument('-f', '--file', help='\U0001F4C2 Specify project folter, '+yellow+'example: /mnt/d/project/vulnerableflask'+reset_text)
 
 
 # Automatically display help if no arguments are provided
@@ -30,15 +31,21 @@ args = parser.parse_args()
 
 
 if args.file:
-    print(f"""[+] Have found files in folder: {args.file}""")
+    print(cyan+f"""[+] Have found files in folder: {args.file}"""+reset_text)
     file_paths=get_all_files(args.file)
-    for path in get_all_files(args.file):
-        print("[-] "+path)
+    for path in file_paths:
+        print("   [-] "+path)
         
     if(os.path.exists(JSON_PATH) == False):
         file_info_to_json(file_paths)
-    print("[+] Searching for vulnerabilities....")
-    time.sleep(2)
+    print("\n[+] Searching for vulnerabilities....")
+    # Wait for the user to press Enter
+    input("\nYour project has been extracted to "+cyan+"data/file_info.json."+reset_text+"\nPress Enter to continue...")
+    
+    #loading bar
+    for i in trange(100, ncols=80):
+        time.sleep(0.01)
+
     for path in get_all_files(args.file):
         sqli(path)
     
